38cae4e81c953cad4119b7c1ce535568dd223aa1
cjp-engr <engr.carmenjoy14@gmail.com>
2020-07-12 11:56:06 +0800

    update signout function

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-side-nav',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  UserSignOut(){
+    this.authService.SignOut();
   }
 
 }
