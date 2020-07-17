1f90190bcd464f80f96c203d080bd63ca5b4d0e1
cjp-engr <engr.carmenjoy14@gmail.com>
2020-07-17 19:53:53 +0800

    nothing to say

--- src/app/shared/services/member.service.ts
+++ src/app/shared/services/member.service.ts
@@ -10,28 +10,21 @@ export class MemberService {
   today = new Date();
 
   constructor(private firestore: AngularFirestore) {}
+  
   form = new FormGroup({
     inputFirstName: new FormControl('', Validators.required),
     inputLastName: new FormControl('', Validators.required),
-    inputAge: new FormControl('', [
-      Validators.pattern('^[0-9]*$'),
-      Validators.max(150),
-    ]),
+    inputAge: new FormControl(null, Validators.required),
     inputGender: new FormControl('', Validators.required),
     inputStatus: new FormControl('', Validators.required),
     inputEmail: new FormControl('', Validators.required),
-    inputMobileNumber: new FormControl('', Validators.required),
+    inputMobileNumber: new FormControl(null, Validators.required),
     inputAddress: new FormControl('', Validators.required),
     inputCity: new FormControl('', Validators.required),
     inputState: new FormControl('', Validators.required),
-    inputZip: new FormControl('', [
-      Validators.pattern('^[0-9]*$'),
-      Validators.maxLength(7),
-    ]),
-    inputDateAndTimeAdded: new FormControl(
-      formatDate(this.today, 'd MMM yyyy hh:mm:ss a', 'en-US')
-    ),
-    memberInformationUpdated: new FormControl(false),
+    inputZip: new FormControl(null, Validators.required),
+    inputDateAndTimeAdded: new FormControl(formatDate(this.today, 'd MMM yyyy hh:mm:ss a', 'en-US')),
+    //memberInformationUpdated: new FormControl(false),
   });
 
   getMemberInformation(): any {
@@ -39,17 +32,30 @@ export class MemberService {
   }
 
   addMemberSubmit() {
-    let data = this.form.value;
-    return new Promise<any>((resolve, reject) => {
+    let data = this.form.getRawValue();
+
+    return this.firestore
+    .collection('members')
+    .add(data).then((e) => {
+      console.log(e.id);
+      console.log('Hi there');
+      })
+      .catch((er) => {
+      console.log(er.message);
+      })
+
+/*     return new Promise<any>((resolve, reject) => {
       this.firestore
         .collection('members')
         .add(data)
         .then(
           (err) => {
+            //eto po ang naglog ng error
+            console.log(data);
             return reject(err);
           }
         );
-    });
+    }); */
   }
 
   clearForm() {
@@ -57,15 +63,16 @@ export class MemberService {
       this.form.reset({
         inputFirstName: '',
         inputLastName: '',
-        inputAge: '',
+        inputAge: null,
         inputGender: '',
         inputStatus: '',
         inputEmail: '',
-        inputMobileNumber: '',
+        inputMobileNumber: null,
         inputAddress: '',
         inputCity: '',
         inputState: '',
-        inputZip: '',
+        inputZip: null,
+        inputDateAndTimeAdded: ''
       });
     }
   }
